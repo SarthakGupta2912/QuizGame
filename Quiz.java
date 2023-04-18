1048b1ea09aeb95d709e5d34f145510e0056a92f
Sarthak Gupta <61643495+SarthakGupta2912@users.noreply.github.com>
2023-04-18 17:41:35 +0530

    Create Quiz.java

---
+++ Quiz.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Quiz {
+
+    static void Question(String str) {
+        System.out.println(str);
+    }
+
+    static void Answer(String key) {
+        Scanner scannerObj = new Scanner(System.in);
+        String str = scannerObj.nextLine();
+        
+        while (!(str.toLowerCase().equals(key.toLowerCase()))) {
+            System.out.println("Wrong Answer, Try again!");
+            str = scannerObj.nextLine();
+        }
+        
+        System.out.println("Correct Answer!");
+    }
+
+    static void UserInput() {
+        Question("What is the capital of India?");
+        Answer("Delhi");
+
+        Question("Who is the president of India?");
+        Answer("Droupadi Murmu");
+        
+
+    }
+
+    public static void main(String args[]) {
+        UserInput();
+    }
+
+}
